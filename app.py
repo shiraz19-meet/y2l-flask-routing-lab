a394930ebcd5ca05c87ca894387dbf37f3d7fb32
shiraz19-meet <shiraz19@meet.mit.edu>
2018-11-03 09:47:58 -0400

    the buttons arent worlinggggg

--- app.py
+++ app.py
@@ -3,7 +3,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_teplate("home.html")
+    return render_template("home.html")
+
+
+@app.route('/shop')
+def shop_page():
+	return render_template("shop.html")
+
+
+
 
 if __name__ == '__main__':
    app.run(debug = True)
+
